69be2a11250515f8230cf89a61f52832853b1a80
Pudyasta Satria <pudyastasatria@gmail.com>
2025-09-02 00:50:42 +0700

    feat[favicon]

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,28 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "VERITAS",
   description: "",
+  icons: {
+    icon: [
+      { url: "/assets/favicon/favicon.ico" },
+      {
+        url: "/assets/favicon/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/assets/favicon/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: "/assets/favicon/apple-touch-icon.png",
+    other: [
+      {
+        rel: "manifest",
+        url: "/assets/favicon/site.webmanifest",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
